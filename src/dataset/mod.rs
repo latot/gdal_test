164e9de8914f368de386a071e948924e0aa88391
latot <{ID}+{username}@users.noreply.github.com>
2025-04-25 18:54:15 -0400

    example access to some vector functions

--- src/dataset/mod.rs
+++ src/dataset/mod.rs
@@ -1,4 +1,31 @@
 pub mod cache;
+pub mod dataset_builder;
 pub mod mode;
 pub mod models;
-pub mod open;
+
+use std::marker::PhantomData;
+
+use core::ffi::c_void;
+
+pub struct Dataset<T> {
+        ds: gdal::Dataset,
+        t: PhantomData<T>,
+}
+
+impl<T> Dataset<T> {
+        fn new(ds: gdal::Dataset) -> Dataset<T> {
+                Dataset { ds, t: PhantomData }
+        }
+        fn c_dataset(&self) -> *mut c_void {
+                self.ds.c_dataset()
+        }
+}
+
+impl Dataset<models::Vector> {
+        fn layer(
+                &self,
+                idx: usize,
+        ) -> std::result::Result<gdal::vector::Layer<'_>, gdal::errors::GdalError> {
+                self.ds.layer(idx)
+        }
+}
